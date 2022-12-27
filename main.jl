a7a554d4019191b06bbabf97122205606177f22f
FluffyCorleone <illuminous9@gmail.com>
2022-12-27 04:42:38 -0500

    Collatz conjecture

--- main.jl
+++ main.jl
@@ -2,8 +2,25 @@
 # following process: If n is even, divide it by 2. If n is odd, multiply it by 3 and add 1.
 
 function collatz( x )
-    (x^2)*2
+    steps = 0
+
+    while true
+
+        if x == 1
+            break
+        end
+
+        if x % 2 == 0
+            x = x / 2
+        else 
+            x = x * 3 + 1
+        end    
+
+        steps = steps + 1
+    end
+
+    steps
 end
 
-y = collatz( 9 )
-println( "Result: $y")
+steps = collatz( 994534 )
+println( "collatz 99: $steps steps")
